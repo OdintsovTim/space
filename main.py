0e233a2649cf43adb9eaa9813715821944772f5b
Odintsov Timur <timodin@yandex.ru>
2019-03-21 00:06:32 +0300

    aded parametrs in terminal

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from instabot import Bot
@@ -8,11 +9,18 @@ from fetch_hubble import download_hubble_images
 from fetch_spacex import fetch_spacex_last_launch
 
 def main():
+    parser = argparse.ArgumentParser(description='This program downloads photos of the last SpaceX rocket launch and from the collection "spacecraft" (you can choose your collection)')
+    parser.add_argument('login')
+    parser.add_argument('password')
+    args = parser.parse_args()
+    login = args.login
+    password = args.password
+
     fetch_spacex_last_launch()
     download_hubble_images('spacecraft')
     myfiles = os.listdir('images')
     bot = Bot()
-    bot.login(username=settings.LOGIN, password=settings.PASSWORD)
+    bot.login(username=login, password=password)
 
     for image in myfiles:
         if image != '.DS_Store':
