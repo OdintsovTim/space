e6f663b318d225a5dc45365e4f23066d681f5eb3
Odintsov Timur <timodin@yandex.ru>
2020-12-05 08:57:31 +0300

    main: fix flake8 warnings

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import os
 
 from instabot import Bot
 
-import settings
-
 from fetch_hubble import download_hubble_images
 from fetch_spacex import fetch_spacex_last_launch
 
+
 def main():
-    parser = argparse.ArgumentParser(description='This program downloads photos of the last SpaceX rocket launch and from the collection "spacecraft" (you can choose your collection)')
+    description = ('This program downloads photos of the last SpaceX rocket '
+                   'launch and from the collection "spacecraft" '
+                   '(you can chose your collection)')
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('login')
     parser.add_argument('password')
     args = parser.parse_args()
@@ -26,5 +28,6 @@ def main():
         if image != '.DS_Store':
             bot.upload_photo(f'images/{image}')
 
+
 if __name__ == "__main__":
-    main()   
+    main()
